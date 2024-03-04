7dd9b68f9e20c49058d036665eb40ebc9cd84d92
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2024-03-04 23:52:33 +0100

    init ui

---
+++ services/frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  let medicine = '';
+  $: console.log('medicine:', medicine);
+</script>
+
+<main>
+  <h1>Welcome to Pharma Pro Link (a.k.a. PPL)</h1>
+
+  <div class="medicine-input">
+    <label for="medicine">Which medicine are you working on?</label>
+    <input id="medicine" type="text" placeholder="e.g., Cosentyx" bind:value={medicine} />
+  </div>
+</main>
