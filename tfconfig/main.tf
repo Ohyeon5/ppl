bc2e584d2444309d203183f8ce3b7a244c6cad6e
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2024-03-05 16:56:58 +0100

    add terraform

--- tfconfig/main.tf
+++ tfconfig/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">=3.0.0"
     }
   }
 }
@@ -15,50 +15,70 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "ppl-rg" {
   name     = "ppl-resources"
-  location = "East US"
+  location = "westus2"
   tags = {
     environment = "dev"
   }
 }
 
-resource "azurerm_virtual_network" "ppl-vn" {
-  name                = "ppl-network"
-  location            = azurerm_resource_group.ppl-rg.location
+resource "azurerm_static_site" "ppl-swa" {
+  name                = "ppl-swa"
   resource_group_name = azurerm_resource_group.ppl-rg.name
-  address_space       = ["10.64.0.0/10"]
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-resource "azurerm_subnet" "ppl-sn" {
-  name                 = "ppl-subnet-1"
-  resource_group_name  = azurerm_resource_group.ppl-rg.name
-  virtual_network_name = azurerm_virtual_network.ppl-vn.name
-  address_prefixes     = ["10.64.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "ppl-nsg" {
-  name                = "ppl-nsg"
   location            = azurerm_resource_group.ppl-rg.location
-  resource_group_name = azurerm_resource_group.ppl-rg.name
+  sku_tier            = "Free"
+  sku_size            = "Free"
 
   tags = {
     environment = "dev"
   }
 }
 
-resource "azurerm_network_security_rule" "ppl-dev-rule" {
-  name                        = "ppl-dev-rule"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.ppl-rg.name
-  network_security_group_name = azurerm_network_security_group.ppl-nsg.name
+output "resource_group_name" {
+  description = "The name of the Resource Group."
+  value       = azurerm_resource_group.ppl-rg.name
+}
+
+output "static_web_app_id" {
+  description = "The ID of the Static Web App."
+  value       = azurerm_static_site.ppl-swa.id
+}
+
+output "static_web_app_default_hostname" {
+  description = "The default hostname associated with the Static Web App."
+  value       = azurerm_static_site.ppl-swa.default_host_name
+}
+
+output "static_web_app_api_key" {
+  description = "The API key of the Static Web App."
+  value       = azurerm_static_site.ppl-swa.api_key
+}
+
+resource "azurerm_service_plan" "ppl-sp" {
+  name                = "ppl-sp"
+  resource_group_name = azurerm_resource_group.ppl-rg.name
+  location            = azurerm_resource_group.ppl-rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_linux_web_app" "ppl-api" {
+  name                = "ppl-api"
+  resource_group_name = azurerm_resource_group.ppl-rg.name
+  location            = azurerm_service_plan.ppl-sp.location
+  service_plan_id     = azurerm_service_plan.ppl-sp.id
+
+  site_config {}
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://utils.azurecr.io/",
+  }
+}
+
+output "web_app_name" {
+  description = "The name of the Web App."
+  value       = azurerm_linux_web_app.ppl-api.name
 }
